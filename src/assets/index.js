7ced8a1c084a09d8d0a95b8732dd08418fca311d
Brown Memoye <memoyebrown@gmail.com>
2023-08-14 16:13:59 +0100

    cleared the readme file

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import heart from './icons/heartIcon.svg';
+import logoIcon from './icons/movieez.svg';
+import tableBg from './backgrounds/tableDesign.svg';
+
+export {
+    logo, heart, logoIcon, tableBg
+}
